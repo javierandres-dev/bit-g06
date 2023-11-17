00139f53a399b092a8300d8dd2009ece0ae88c73
Javier Andrés <55107141+javierandres-dev@users.noreply.github.com>
2023-11-16 22:04:37 -0500

    Acerca de JavaScript.

--- docs/javascript/ejercicios.js
+++ docs/javascript/ejercicios.js
@@ -8,3 +8,23 @@ alert('Hola, ' + nombre);
 /* Preguntar nombre al usuario y saludarlo por su nombre.
 Si el usuario no escribe su nombre saludar: "Hola, desconocido."
 */
+/*
+const nombre = prompt('Por favor ingrese su nombre:');
+if (nombre) {
+  document.write('hola, ' + nombre);
+} else {
+  document.write('hola, desconocido.');
+}
+*/
+
+// Leer nombre y apellido, saludar por nombre y apellido.
+// Pregunte al usuario si desea continuar, responder con hola o adiós.
+// Leer nombre y edad, Mostrar si es o no mayor de edad.
+// Leer un rol, (admin, ventas, cliente), acceso de acuerdo al rol.
+// Leer un número, mostrar el doble de n.
+// Leer un número, mostrar la mitad de n.
+// Leer un número, mostrar si es positivo, negativo o cero.
+// Leer un número, mostrar si es par o impar.
+// Leer dos números, mostrar el mayor.
+// Leer dos números, mostrar operaciones básicas (+,-,x,/,%,cuadrado).
+// Leer dos números, mostrar la suma dividido en la resta.
