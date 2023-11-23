1e893bf347c4a16fd0a0d54662c3a32e1d6e6ee1
Javier Andrés <55107141+javierandres-dev@users.noreply.github.com>
2023-11-22 21:58:51 -0500

    Acerca de JavaScript.

--- docs/javascript/ejercicios.js
+++ docs/javascript/ejercicios.js
@@ -1,9 +1,11 @@
 'use strict';
 // Preguntar nombre al usuario y saludarlo por su nombre.
+/*
 function saludarUsuario(nombre) {
   document.write(`Hola, ${nombre}`);
 }
-//saludarUsuario(prompt('Por favor ingrese su nombre:'));
+saludarUsuario(prompt('Por favor ingrese su nombre:'));
+*/
 
 /* Preguntar nombre al usuario y saludarlo por su nombre.
 Si el usuario no escribe su nombre saludar: "Hola, desconocido."
@@ -16,10 +18,28 @@ if (nombre) {
   document.write('hola, desconocido.');
 }
 */
+
 // Leer nombre y apellido, saludar por nombre y apellido.
 // Pregunte al usuario si desea continuar, responder con hola o adiós.
 // Leer nombre y edad, Mostrar si es o no mayor de edad.
 // Leer un rol, (admin, ventas, cliente), acceso de acuerdo al rol.
+/*
+function accesoXrol(rol) {
+  switch (rol) {
+    case 'admin':
+      return 'Acceso a la zona de administradores.';
+    case 'ventas':
+      return 'Acceso a la zona de vendedores.';
+    case 'cliente':
+      return 'Acceso a la zona de clientes.';
+    default:
+      return 'Acceso no autorizado.';
+  }
+}
+const retornado = accesoXrol(prompt('Por favor escriba un rol:'));
+document.write(retornado);
+*/
+
 // Leer un número, mostrar el doble de n.
 // Leer un número, mostrar la mitad de n.
 // Leer un número, mostrar si es positivo, negativo o cero.
@@ -27,3 +47,11 @@ if (nombre) {
 // Leer dos números, mostrar el mayor.
 // Leer dos números, mostrar operaciones básicas (+,-,x,/,%,cuadrado).
 // Leer dos números, mostrar la suma dividido en la resta.
+// Pedir un número y mostrar los números pares desde 1 hasta n.
+// Pedir un número, mostrar la suma total desde 1 hasta n.
+// Pedir un número, mostrar la suma de los números pares desde 1 hasta n.
+// Pedir un número, mostrar la suma de los números impares desde 1 hasta n.
+// Pedir un número, mostrar multiplos de 3 desde 1 hasta n.
+// Pedir un número, mostrar los números desde n hasta 0.
+// Pedir un número, mostrar los números desde 0 hasta n.
+// Pedir un número, mostrar su tabla de multiplicar desde 0 hasta 10.
