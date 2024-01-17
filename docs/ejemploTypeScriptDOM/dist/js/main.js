6871984b2784697e3fac7bd4ef5a751bd0f06bb9
javi <55107141+javierandres-dev@users.noreply.github.com>
2024-01-16 20:09:51 -0500

    Ejemplo TypeScript DOM

--- docs/ejemploTypeScriptDOM/dist/js/main.js
+++ docs/ejemploTypeScriptDOM/dist/js/main.js
@@ -1,6 +1,12 @@
 "use strict";
 const $form = document.querySelector('form');
 $form.addEventListener('submit', agregarEmpleado);
+$form.nacimiento.addEventListener('input', (evento) => {
+    var _a;
+    const edadCalculada = obtenerEdad((_a = evento === null || evento === void 0 ? void 0 : evento.target) === null || _a === void 0 ? void 0 : _a.value);
+    $form.edad.value =
+        edadCalculada === 1 ? `${edadCalculada} año` : `${edadCalculada} años`;
+});
 const empleados = [];
 function agregarEmpleado(evento) {
     evento.preventDefault();
@@ -15,13 +21,55 @@ function agregarEmpleado(evento) {
         telefono: $form.telefono.value,
         correo: $form.correo.value,
         web: $form.web.value,
-        vivienda: $form.checked,
-        vehiculo: $form.checked,
+        vivienda: $form.vivienda.checked,
+        vehiculo: $form.vehiculo.checked,
         climaPreferido: $form.clima.value,
         horaAlmuerzo: $form.almuerzo.value,
-        colorFavorito: $form.color.value,
+        colorFavorito: $form.color.value || '#ffffff',
         pasatiempos: $form.pasatiempos.value.split(','),
         salario: $form.salario.value,
     };
-    console.log(empleado);
+    empleados.push(empleado);
+    $form.reset();
+    mostrarEmpleados();
+}
+function obtenerEdad(fecha) {
+    const hoy = new Date();
+    const fechaNacimiento = new Date(fecha);
+    const diferenciaAnios = hoy.getFullYear() - fechaNacimiento.getFullYear();
+    const esAntesDelNacimiento = hoy.getMonth() < fechaNacimiento.getMonth() ||
+        (hoy.getMonth() === fechaNacimiento.getMonth() &&
+            hoy.getDate() < fechaNacimiento.getDate());
+    return esAntesDelNacimiento ? diferenciaAnios - 1 : diferenciaAnios;
+}
+function mostrarEmpleados() {
+    const $tbody = document.querySelector('tbody');
+    let contenidoHtml = '';
+    empleados.forEach((empleado) => {
+        const edadEmpleado = obtenerEdad(empleado.nacimiento);
+        contenidoHtml += `
+<tr>
+  <td>${empleado.nombre} ${empleado.apellido}</td>
+  <td>${edadEmpleado === 1 ? `${edadEmpleado} año` : `${edadEmpleado} años`}</td>
+  <td>${empleado.genero}</td>
+  <td>${empleado.estadoCivil}</td>
+  <td>${empleado.cantidadHijos}</td>
+  <td>${empleado.telefono}</td>
+  <td>${empleado.correo}</td>
+  <td>${empleado.web}</td>
+  <td>${empleado.vivienda ? 'Si' : 'No'}</td>
+  <td>${empleado.vehiculo ? 'Si' : 'No'}</td>
+  <td>${empleado.climaPreferido === '0'
+            ? 'Frio'
+            : empleado.climaPreferido === '2'
+                ? 'Cálido'
+                : 'Templado'}</td>
+  <td>${empleado.horaAlmuerzo}</td>
+  <td class="circulo" style="background-color: ${empleado.colorFavorito}"></td>
+  <td>${empleado.pasatiempos}</td>
+  <td>${empleado.salario}</td>
+</tr>
+    `;
+    });
+    $tbody.innerHTML = contenidoHtml;
 }
--- docs/ejemploTypeScriptDOM/src/main.ts
+++ docs/ejemploTypeScriptDOM/src/main.ts
@@ -1,6 +1,11 @@
 const $form = document.querySelector('form') as HTMLFormElement;
 
 $form.addEventListener('submit', agregarEmpleado);
+$form.nacimiento.addEventListener('input', (evento: Event) => {
+  const edadCalculada = obtenerEdad(evento?.target?.value);
+  $form.edad.value =
+    edadCalculada === 1 ? `${edadCalculada} año` : `${edadCalculada} años`;
+});
 
 interface Empleado {
   id: string;
@@ -37,13 +42,62 @@ function agregarEmpleado(evento: Event): void {
     telefono: $form.telefono.value,
     correo: $form.correo.value,
     web: $form.web.value,
-    vivienda: $form.checked,
-    vehiculo: $form.checked,
+    vivienda: $form.vivienda.checked,
+    vehiculo: $form.vehiculo.checked,
     climaPreferido: $form.clima.value,
     horaAlmuerzo: $form.almuerzo.value,
-    colorFavorito: $form.color.value,
+    colorFavorito: $form.color.value || '#ffffff',
     pasatiempos: $form.pasatiempos.value.split(','),
     salario: $form.salario.value,
   };
-  console.log(empleado);
+  empleados.push(empleado);
+  $form.reset();
+  mostrarEmpleados();
+}
+
+function obtenerEdad(fecha: Date): number {
+  const hoy = new Date();
+  const fechaNacimiento = new Date(fecha);
+  const diferenciaAnios = hoy.getFullYear() - fechaNacimiento.getFullYear();
+  const esAntesDelNacimiento =
+    hoy.getMonth() < fechaNacimiento.getMonth() ||
+    (hoy.getMonth() === fechaNacimiento.getMonth() &&
+      hoy.getDate() < fechaNacimiento.getDate());
+  return esAntesDelNacimiento ? diferenciaAnios - 1 : diferenciaAnios;
+}
+
+function mostrarEmpleados(): void {
+  const $tbody = document.querySelector('tbody')!;
+  let contenidoHtml: string = '';
+  empleados.forEach((empleado) => {
+    const edadEmpleado: number = obtenerEdad(empleado.nacimiento);
+    contenidoHtml += `
+<tr>
+  <td>${empleado.nombre} ${empleado.apellido}</td>
+  <td>${
+    edadEmpleado === 1 ? `${edadEmpleado} año` : `${edadEmpleado} años`
+  }</td>
+  <td>${empleado.genero}</td>
+  <td>${empleado.estadoCivil}</td>
+  <td>${empleado.cantidadHijos}</td>
+  <td>${empleado.telefono}</td>
+  <td>${empleado.correo}</td>
+  <td>${empleado.web}</td>
+  <td>${empleado.vivienda ? 'Si' : 'No'}</td>
+  <td>${empleado.vehiculo ? 'Si' : 'No'}</td>
+  <td>${
+    empleado.climaPreferido === '0'
+      ? 'Frio'
+      : empleado.climaPreferido === '2'
+      ? 'Cálido'
+      : 'Templado'
+  }</td>
+  <td>${empleado.horaAlmuerzo}</td>
+  <td class="circulo" style="background-color: ${empleado.colorFavorito}"></td>
+  <td>${empleado.pasatiempos}</td>
+  <td>${empleado.salario}</td>
+</tr>
+    `;
+  });
+  $tbody.innerHTML = contenidoHtml;
 }
