22c5dddf6f8a48e0f38b917d42e7708fc68f84f2
javi <55107141+javierandres-dev@users.noreply.github.com>
2024-02-09 22:02:16 -0500

    Ejemplo Backend

--- docs/ejemploBackend/src/helpers/funciones.js
+++ docs/ejemploBackend/src/helpers/funciones.js
@@ -1,8 +1,10 @@
 import jwt from 'jsonwebtoken';
 
+const llaveSecreta = process.env.SECRET_KEY;
+
 export function generarToken(payload) {
   return new Promise((resolver, rechazar) => {
-    jwt.sign(payload, 'llaveSecreta', { expiresIn: '1h' }, (error, token) => {
+    jwt.sign(payload, llaveSecreta, { expiresIn: '1h' }, (error, token) => {
       if (error) {
         rechazar({ error });
       } else {
@@ -14,7 +16,7 @@ export function generarToken(payload) {
 
 export function verificarToken(token) {
   return new Promise((resolver, rechazar) => {
-    jwt.verify(token, 'llaveSecreta', (error, decodificado) => {
+    jwt.verify(token, llaveSecreta, (error, decodificado) => {
       if (error) {
         rechazar(error);
       } else {
